3b4397b5634ec5bbfde8434ba3bcf51781af3b18
akasaka-h <81917758+akasaka-h@users.noreply.github.com>
2022-02-04 01:37:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def send_line_notify(notification_message):
     """
     LINEに通知する
     """
-    line_notify_token = 'sqeTArSjsznpEiyaixwSsJkMxW9iXxcuKmF77AZK38Q'
+    line_notify_token = '自分のtoken'
     line_notify_api = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': f'Bearer {line_notify_token}'}
     data = {'message': f'message: {notification_message}'}
